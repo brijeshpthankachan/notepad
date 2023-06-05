4e764780a199729bd85d21455ba38588fe8d23e6
jain varghese <jain.varghese@experionglobal.com>
2023-06-05 15:32:29 +0530

    Added components

---
+++ src/app/components/note/note.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-note',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './note.component.html',
+  styleUrls: ['./note.component.scss']
+})
+export class NoteComponent {
+
+}
