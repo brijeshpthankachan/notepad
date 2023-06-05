32e39b17df7c7af3042891172272972d23a27122
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-06-05 21:55:53 +0530

    Added mui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeComponentComponent } from './components/home-component/home-component.component'
@@ -9,6 +10,11 @@ import { HomeComponentComponent } from './components/home-component/home-compone
 	declarations: [AppComponent],
 	providers: [],
 	bootstrap: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, HomeComponentComponent]
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HomeComponentComponent,
+		BrowserAnimationsModule
+	]
 })
 export class AppModule {}
--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,10 +1,15 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { MatButtonModule } from '@angular/material/button'
+import { MatDialogModule } from '@angular/material/dialog'
 
 @Component({
 	selector: 'app-editor',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, MatDialogModule, MatButtonModule, FormsModule],
 	templateUrl: './editor.component.html'
 })
-export class EditorComponent {}
+export class EditorComponent {
+	value: string
+}
