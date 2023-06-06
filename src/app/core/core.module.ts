e1484831d441ecc05288eff366ad7721be555928
jain varghese <jain.varghese@experionglobal.com>
2023-06-06 16:05:29 +0530

    highlight

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,18 +2,15 @@ import { NgModule } from '@angular/core'
 
 import { CommonModule } from '@angular/common'
 import { DateRangeFilterPipe } from '../Pipe/dateRangeFilter'
-import { DeleteFilterPipe } from '../Pipe/deleteFilter'
 import { HighlightDirective } from '../directives/highlight.directive'
 
 @NgModule({
 	declarations: [
 		DateRangeFilterPipe,
-		DeleteFilterPipe,
 		HighlightDirective,
-		DeleteFilterPipe
 	],
 
 	imports: [CommonModule],
-	exports: [DateRangeFilterPipe, DeleteFilterPipe, HighlightDirective]
+	exports: [DateRangeFilterPipe, HighlightDirective]
 })
 export class CoreModule {}
