8d7f91fbc0baf2205af06afb2245066e92ddff6b
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-06-07 09:45:02 +0530

    changed to modern color notation

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -7,11 +7,11 @@ export class HighlightDirective {
 	constructor(private el: ElementRef) {}
 
 	@HostListener('mouseenter') onMouseEnter() {
-		this.highlight('rgb(153, 243, 150)')
+		this.highlight('rgb(153 243 150)')
 	}
 
 	@HostListener('mouseleave') onMouseLeave() {
-		this.highlight('rgb(153, 243, 178)')
+		this.highlight('rgb(153 243 178)')
 	}
 
 	private highlight(color: string) {
