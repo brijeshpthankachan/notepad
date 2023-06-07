6acce8c790b54019a5be95aaab4bc26c80ace02d
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-06-07 09:47:51 +0530

    ert

--- src/app/data/Notes.ts
+++ src/app/data/Notes.ts
@@ -13,17 +13,7 @@ export const note: INote[] = [
 	{
 		id: 2,
 		title: 'Shopping List',
-		content: `
-			 1. Milk - 2 liters 
-			 2. Bread - Whole wheat
-			 3. Eggs - A dozen
-			 4. Chicken - Boneless, skinless
-			 5. Tomatoes - 4 medium-sized
-			 6. Onions - 2 large
-			 7. Pasta - Penne
-			 8. Cheese - Cheddar
-			 9. Apples - 6
-			 10. Yogurt - Greek, plain`,
+		content: `1. Milk - 2 liters 2. Bread - Whole wheat3. Eggs - A dozen 4. Chicken - Boneless, skinless 5. Tomatoes - 4 medium-sized 6. Onions - 2 large 7. Pasta - Penne 8. Cheese - Cheddar 9. Apples - 6 10. Yogurt - Greek, plain`,
 		writtenOn: new Date('2023-06-02'),
 		isDeleted: false
 	},
@@ -39,7 +29,7 @@ export const note: INote[] = [
 		id: 4,
 		title: 'Travel Itinerary',
 		content:
-			'Destination: Paris, France\nDuration: 7 days\n\nDay 1: Visit the Eiffel Tower and enjoy a boat cruise on the Seine River.\nDay 2: Explore the Louvre Museum and indulge in French cuisine.\nDay 3: Take a day trip to Versailles and tour the magnificent palace.\nDay 4: Discover the charm of Montmartre and visit Sacred-Chur Basilica.\nDay 5: Stroll along the Champs-Elysée and visit the Arc de Triumphed.\nDay 6: Experience the beauty of Notre-Dame Cathedral and take a river cruise.\nDay 7: Enjoy a relaxing day in the Luxembourg Gardens before departure.',
+			'Destination: Paris, France  Duration: 7 days    Day 1: Visit the Eiffel Tower and enjoy a boat cruise on the Seine River.  Day 2: Explore the Louvre Museum and indulge in French cuisine.  Day 3: Take a day trip to Versailles and tour the magnificent palace.  Day 4: Discover the charm of Montmartre and visit Sacred-Chur Basilica.  Day 5: Stroll along the Champs-Elysée and visit the Arc de Triumphed.  Day 6: Experience the beauty of Notre-Dame Cathedral and take a river cruise.  Day 7: Enjoy a relaxing day in the Luxembourg Gardens before departure.',
 		writtenOn: new Date('2023-05-30'),
 		isDeleted: false
 	},
@@ -47,7 +37,7 @@ export const note: INote[] = [
 		id: 5,
 		title: 'Fitness Goals',
 		content:
-			'Current weight: 70 kg\nGoal weight: 65 kg\n\nTo achieve my fitness goals, I will:\n- Follow a balanced diet with reduced calorie intake.\n- Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).\n- Drink plenty of water and avoid sugary beverages.\n- Track my progress regularly and make adjustments as needed.\n\nI aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
+			'Current weight: 70 kg  Goal weight: 65 kg    To achieve my fitness goals, I will:  - Follow a balanced diet with reduced calorie intake.  - Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).  - Drink plenty of water and avoid sugary beverages.  - Track my progress regularly and make adjustments as needed.    I aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
 		writtenOn: new Date('2023-06-04'),
 		isDeleted: false
 	},
@@ -55,7 +45,7 @@ export const note: INote[] = [
 		id: 6,
 		title: 'Fness Goals',
 		content:
-			'Current weight: 70 kg\nGoal weight: 65 kg\n\nTo achieve my fitness goals, I will:\n- Follow a balanced diet with reduced calorie intake.\n- Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).\n- Drink plenty of water and avoid sugary beverages.\n- Track my progress regularly and make adjustments as needed.\n\nI aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
+			'Current weight: 70 kg  Goal weight: 65 kg    To achieve my fitness goals, I will:  - Follow a balanced diet with reduced calorie intake.  - Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).  - Drink plenty of water and avoid sugary beverages.  - Track my progress regularly and make adjustments as needed.    I aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
 		writtenOn: new Date('2023-06-04'),
 		isDeleted: false
 	},
@@ -63,7 +53,7 @@ export const note: INote[] = [
 		id: 7,
 		title: 'Fitness ls',
 		content:
-			'Current weight: 70 kg\nGoal weight: 65 kg\n\nTo achieve my fitness goals, I will:\n- Follow a balanced diet with reduced calorie intake.\n- Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).\n- Drink plenty of water and avoid sugary beverages.\n- Track my progress regularly and make adjustments as needed.\n\nI aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
+			'Current weight: 70 kg  Goal weight: 65 kg    To achieve my fitness goals, I will:  - Follow a balanced diet with reduced calorie intake.  - Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).  - Drink plenty of water and avoid sugary beverages.  - Track my progress regularly and make adjustments as needed.    I aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
 		writtenOn: new Date('2023-06-04'),
 		isDeleted: false
 	},
@@ -71,7 +61,7 @@ export const note: INote[] = [
 		id: 8,
 		title: 'Fiess Goals',
 		content:
-			'Current weight: 70 kg\nGoal weight: 65 kg\n\nTo achieve my fitness goals, I will:\n- Follow a balanced diet with reduced calorie intake.\n- Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).\n- Drink plenty of water and avoid sugary beverages.\n- Track my progress regularly and make adjustments as needed.\n\nI aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
+			'Current weight: 70 kg  Goal weight: 65 kg    To achieve my fitness goals, I will:  - Follow a balanced diet with reduced calorie intake.  - Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).  - Drink plenty of water and avoid sugary beverages.  - Track my progress regularly and make adjustments as needed.    I aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
 		writtenOn: new Date('2023-06-04'),
 		isDeleted: false
 	},
@@ -79,7 +69,7 @@ export const note: INote[] = [
 		id: 9,
 		title: 'ness Goals',
 		content:
-			'Current weight: 70 kg\nGoal weight: 65 kg\n\nTo achieve my fitness goals, I will:\n- Follow a balanced diet with reduced calorie intake.\n- Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).\n- Drink plenty of water and avoid sugary beverages.\n- Track my progress regularly and make adjustments as needed.\n\nI aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
+			'Current weight: 70 kg  Goal weight: 65 kg    To achieve my fitness goals, I will:  - Follow a balanced diet with reduced calorie intake.  - Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).  - Drink plenty of water and avoid sugary beverages.  - Track my progress regularly and make adjustments as needed.    I aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
 		writtenOn: new Date('2023-06-04'),
 		isDeleted: false
 	},
@@ -87,7 +77,7 @@ export const note: INote[] = [
 		id: 10,
 		title: 'Fitns Goals',
 		content:
-			'Current weight: 70 kg\nGoal weight: 65 kg\n\nTo achieve my fitness goals, I will:\n- Follow a balanced diet with reduced calorie intake.\n- Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).\n- Drink plenty of water and avoid sugary beverages.\n- Track my progress regularly and make adjustments as needed.\n\nI aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
+			'Current weight: 70 kg  Goal weight: 65 kg    To achieve my fitness goals, I will:  - Follow a balanced diet with reduced calorie intake.  - Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).  - Drink plenty of water and avoid sugary beverages.  - Track my progress regularly and make adjustments as needed.    I aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
 		writtenOn: new Date('2023-06-06'),
 		isDeleted: false
 	},
@@ -95,7 +85,7 @@ export const note: INote[] = [
 		id: 11,
 		title: 'Fitness Go',
 		content:
-			'Current weight: 70 kg\nGoal weight: 65 kg\n\nTo achieve my fitness goals, I will:\n- Follow a balanced diet with reduced calorie intake.\n- Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).\n- Drink plenty of water and avoid sugary beverages.\n- Track my progress regularly and make adjustments as needed.\n\nI aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
+			'Current weight: 70 kg  Goal weight: 65 kg    To achieve my fitness goals, I will:  - Follow a balanced diet with reduced calorie intake.  - Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).  - Drink plenty of water and avoid sugary beverages.  - Track my progress regularly and make adjustments as needed.    I aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
 		writtenOn: new Date('2023-06-06'),
 		isDeleted: false
 	},
@@ -103,7 +93,7 @@ export const note: INote[] = [
 		id: 12,
 		title: 'Fitness ',
 		content:
-			'Current weight: 70 kg\nGoal weight: 65 kg\n\nTo achieve my fitness goals, I will:\n- Follow a balanced diet with reduced calorie intake.\n- Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).\n- Drink plenty of water and avoid sugary beverages.\n- Track my progress regularly and make adjustments as needed.\n\nI aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
+			'Current weight: 70 kg  Goal weight: 65 kg    To achieve my fitness goals, I will:  - Follow a balanced diet with reduced calorie intake.  - Exercise for at least 30 minutes, **five** days a week (combination of cardio and strength training).  - Drink plenty of water and avoid sugary beverages.  - Track my progress regularly and make adjustments as needed.    I aim to reach my goal weight within three months and maintain a healthy lifestyle thereafter.',
 		writtenOn: new Date('2023-06-06'),
 		isDeleted: false
 	}
