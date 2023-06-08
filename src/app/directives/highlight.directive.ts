f816b2a3e12e21ad4d46b904b95e9accc8886bb7
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-06-08 14:08:26 +0530

    edited colors

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -6,14 +6,14 @@ import { Directive, ElementRef, HostListener, OnInit } from '@angular/core'
 export class HighlightDirective implements OnInit {
 	constructor(private el: ElementRef) { }
 	color: string
-	colors = ['#FEFAE0', '#E9EDC9', '#FAEDCD', '#DBE4C6', '#F1DEC9', '#EEEEEE']
+	colors = ['#F7E6C4', '#F5EFE7', '#FFE7CE', '#FCE9F1', '#ECF8F9', '#F8E8EE', '#E3F4F4', '#E6FFFD', '#F9FBE7', '#E8F6EF']
 
 	getRandomInteger(min: number, max: number): number {
 		return Math.floor(Math.random() * (max - min + 1)) + min
 	}
 
 	ngOnInit(): void {
-		const randomNum: number = this.getRandomInteger(0, 5)
+		const randomNum: number = this.getRandomInteger(0, 9)
 		this.color = this.colors[randomNum]
 		this.highlight(this.color)
 	}
